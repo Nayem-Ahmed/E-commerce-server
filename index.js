8b5a376ca614ab3b626f365032e42a098755c506
unknown <freelancernayem.me@gmail.comm>
2024-03-27 16:10:53 +0600

    data post and fetch

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.8wqrrau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,17 +27,26 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         const allproductsCollection = client.db('E-commerce').collection('allproducts')
+        const addTocartCollection = client.db('E-commerce').collection('addCart')
 
 
-        // Get all wishlist
+        // Get allproducts
         app.get('/allproducts', async (req, res) => {
             const result = await allproductsCollection.find().toArray()
             res.send(result)
         })
-
-
-
-
+        // Get single products
+        app.get('/allproducts/:id', async (req, res) => {
+            const id = req.params.id
+            const result = await allproductsCollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+        //  add cart post
+        app.post('/addcart', async (req, res) => {
+            const addcart = req.body;
+            const result = await addTocartCollection.insertOne(addcart);
+            res.send(result);
+        })
 
         // await client.connect();
         // Send a ping to confirm a successful connection
